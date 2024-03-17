c5434645b171f150d51212536127c4a72ec5cdf1
Jarnil Patel <pateljarnil@gmail.com>
2024-03-17 14:31:29 +0530

    Added checkbox in contact us page.

--- src/app/components/amenities/amenities.component.ts
+++ src/app/components/amenities/amenities.component.ts
@@ -32,16 +32,6 @@ export class AmenitiesComponent {
         numVisible: 3,
         numScroll: 3,
       },
-      {
-        breakpoint: '650px',
-        numVisible: 2,
-        numScroll: 2,
-      },
-      {
-        breakpoint: '515px',
-        numVisible: 1,
-        numScroll: 1,
-      },
     ];
   }
 

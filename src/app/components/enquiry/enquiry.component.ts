4dc2d6f7cf22df571210361b90329abbdd8881ab
Ansh892001 <104520425+Ansh892001@users.noreply.github.com>
2024-03-24 11:51:51 +0530

    changes regarding enquiry form

--- src/app/components/enquiry/enquiry.component.ts
+++ src/app/components/enquiry/enquiry.component.ts
@@ -2,6 +2,24 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 
+class EnquiryData {
+  name!: string;
+  contact!: string;
+  email!: string;
+  occupation: string;
+  address: string;
+  requirements: Array<String>;
+
+  constructor() {
+    this.name = '';
+    this.contact = '';
+    this.occupation = '';
+    this.email = '';
+    this.address = '';
+    this.requirements = [];
+  }
+}
+
 @Component({
   selector: 'app-enquiry',
   templateUrl: './enquiry.component.html',
@@ -9,30 +27,58 @@ import { MessageService } from 'primeng/api';
 })
 export class EnquiryComponent {
   visible: boolean = false;
-  enquiryForm!: FormGroup;
-  requirements: string[] = [];
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private messageService: MessageService
-  ) {}
+  optionsArray: Array<string> = [];
+  public enquiryData!: EnquiryData;
+  public isSubmit: boolean = false;
+  constructor(private messageService: MessageService) {}
 
   ngOnInit(): void {
-    this.enquiryForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      contactNumber: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      occupation: [''],
-      address: [''],
-      requirements: [[], Validators.minLength(1)],
-    });
+    this.optionsArray = ['2BHK', '3BHK', 'SHOPS'];
+    this.enquiryData = new EnquiryData();
+  }
+
+  addRemoveOptions(option: string) {
+    let requirements = this.enquiryData.requirements;
+    if (!requirements.includes(option)) {
+      requirements.push(option);
+    } else {
+      let index = requirements.indexOf(option);
+      requirements.splice(index, 1);
+    }
+  }
+
+  isNameInvalid() {
+    if (this.enquiryData.name == '' && this.isSubmit) return true;
+    return false;
+  }
+
+  isEmailInvalid() {
+    if (this.enquiryData.name == '' && this.isSubmit) return true;
+    return false;
+  }
+
+  isContactInvalid() {
+    if (this.enquiryData.name == '' && this.isSubmit) return true;
+    return false;
+  }
+
+  validateData() {
+    if (
+      this.enquiryData.name == '' ||
+      this.enquiryData.contact == '' ||
+      this.enquiryData.email == '' ||
+      this.enquiryData.requirements.length <= 0 ||
+      this.enquiryData.requirements.length > 3
+    ) {
+      return false;
+    }
+    return true;
   }
 
   onSubmit() {
-    this.enquiryForm.markAllAsTouched();
-    this.enquiryForm.updateValueAndValidity();
-    if (this.enquiryForm.valid) {
-      console.log('Form values:', this.enquiryForm.value);
+    this.isSubmit = true;
+    if (this.validateData()) {
+      console.log('Form values:', this.enquiryData);
       // this.downloadBrochure();
       this.messageService.add({
         severity: 'success',
@@ -44,7 +90,7 @@ export class EnquiryComponent {
       this.messageService.add({
         severity: 'info',
         summary: 'Please fill out required fields.',
-        detail: 'Name, Contact Number, Email, Requirement',
+        detail: 'Name, Contact Number, Email And Requirement',
       });
     }
   }

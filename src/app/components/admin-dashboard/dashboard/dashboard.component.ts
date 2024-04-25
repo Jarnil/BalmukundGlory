ad35958230911c650fd4d79ba7a2d3f17e97ca71
Jarnil Patel <pateljarnil@gmail.com>
2024-04-25 23:34:18 +0530

    Worked on the dashboard component.

--- src/app/components/admin-dashboard/dashboard/dashboard.component.ts
+++ src/app/components/admin-dashboard/dashboard/dashboard.component.ts
@@ -11,12 +11,12 @@ import { Console } from 'console';
 })
 export class DashboardComponent implements OnInit {
   enquiries: any;
-
+  dateRange!: Date[];
   enquiryList: EnquiryList = {
     pageNo: 0,
-    pageSize: 10,
-    sortBy: 'id',
-    sortOrder: 'asc',
+    pageSize: 100,
+    sortBy: '',
+    sortOrder: '',
     columnSearchValue: {
       searchName: '',
       searchContactNumber: '',
@@ -39,6 +39,22 @@ export class DashboardComponent implements OnInit {
   }
 
   getEnquiryList() {
+    if (
+      this.enquiryList.columnSearchValue.startDate &&
+      this.enquiryList.columnSearchValue.endDate
+    ) {
+      const formattedStartDate = this.formatDate(
+        this.enquiryList.columnSearchValue.startDate
+      );
+      const formattedEndDate = this.formatDate(
+        this.enquiryList.columnSearchValue.endDate,
+        true
+      );
+
+      this.enquiryList.columnSearchValue.startDate = formattedStartDate;
+      this.enquiryList.columnSearchValue.endDate = formattedEndDate;
+    }
+
     this.enquiryService.getEnquiries(this.enquiryList).subscribe(
       (response) => {
         this.enquiries = response.data.data;
@@ -59,21 +75,63 @@ export class DashboardComponent implements OnInit {
     switch (requirement.toLowerCase()) {
       case '2bhk':
         return {
-          background: '#427D9D',
+          background: '#265073',
         };
       case '3bhk':
         return {
-          background: '#164863',
+          background: '#818FB4',
         };
       case 'shop':
         return {
-          background: '#3876BF',
+          background: '#19A7CE',
         };
     }
   }
 
+  updateDateRange() {
+    console.log(this.dateRange);
+    if (this.dateRange && this.dateRange.length === 2) {
+      // Set start date and end date in the enquiryList object
+      this.enquiryList.columnSearchValue.startDate =
+        this.dateRange[0].toLocaleDateString();
+      this.enquiryList.columnSearchValue.endDate =
+        this.dateRange[1].toLocaleDateString();
+    } else {
+      // Clear start date and end date if range is not selected
+      this.enquiryList.columnSearchValue.startDate = '';
+      this.enquiryList.columnSearchValue.endDate = '';
+    }
+  }
+
+  formatDate(dateString: string, isEndDate = false) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = isEndDate ? '23' : '00';
+    const minutes = isEndDate ? '59' : '00';
+    const seconds = isEndDate ? '59' : '00';
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
   onReset() {
-    console.log('reset');
-    // this.getEnquiryList();
+    this.enquiryList = {
+      pageNo: 0,
+      pageSize: 100,
+      sortBy: 'id',
+      sortOrder: 'asc',
+      columnSearchValue: {
+        searchName: '',
+        searchContactNumber: '',
+        searchEmail: '',
+        searchAddress: '',
+        searchOccupation: '',
+        searchRequirement: '',
+        startDate: '',
+        endDate: '',
+      },
+    };
+    this.getEnquiryList();
   }
 }
--- src/app/components/admin-dashboard/login/login.component.ts
+++ src/app/components/admin-dashboard/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';

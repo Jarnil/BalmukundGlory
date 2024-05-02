f8ab86a07d5b560747fbeca612d8bd3e6d6ba5f7
Jarnil Patel <pateljarnil@gmail.com>
2024-05-02 23:41:21 +0530

    Did the integration of the charts.

--- src/app/components/admin-dashboard/admin-dashboard.module.ts
+++ src/app/components/admin-dashboard/admin-dashboard.module.ts
@@ -8,11 +8,25 @@ import { CommonModule } from '@angular/common';
 import { AdminDashboardRoutingModule } from './admin-dashboard-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SharedModule } from '../../shared/shared.module';
+import { NgxEchartsModule } from 'ngx-echarts';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EnquiryTableComponent } from './enquiry-table/enquiry-table.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @NgModule({
-  declarations: [DashboardComponent, SidebarComponent, EnquiryTableComponent],
-  imports: [CommonModule, AdminDashboardRoutingModule, SharedModule],
+  declarations: [
+    DashboardComponent,
+    SidebarComponent,
+    EnquiryTableComponent,
+    BarChartComponent,
+  ],
+  imports: [
+    CommonModule,
+    AdminDashboardRoutingModule,
+    SharedModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'),
+    }),
+  ],
 })
 export class AdminDashboardModule {}
---
+++ src/app/interface/Chart.ts
@@ -0,0 +1,8 @@
+export interface ChartRequest {
+  startDate: Date;
+  endDate: Date;
+}
+export interface PieChartData {
+  value: number;
+  name: string;
+}
